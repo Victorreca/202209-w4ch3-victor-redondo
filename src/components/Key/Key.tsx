5e65700432b6ea1b0fbc38805ad4abf976a39b66
Victor Redondo <vrviktor@gmail.com>
2022-10-20 20:13:35 +0200

    Add number to display

---
+++ src/components/Key/Key.tsx
@@ -0,0 +1,15 @@
+interface KeyProps {
+  text: string;
+  className: string;
+  action: () => void;
+}
+
+const Key = ({ text, className, action }: KeyProps): JSX.Element => {
+  return (
+    <button className={className} onClick={action}>
+      {text}
+    </button>
+  );
+};
+
+export default Key;
--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,41 +1,29 @@
 import "./Keyboard.css";
+import Key from "../Key/Key";
+import PhoneContext from "../../context/PhoneContext";
+import { useContext } from "react";
 
 const Keyboard = (): JSX.Element => {
+  const keyboardValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  const { addNumber } = useContext(PhoneContext);
+
   return (
     <div className="keyboard-container">
       <ol className="keyboard">
-        <li>
-          <button className="key">1</button>
-        </li>
-        <li>
-          <button className="key">2</button>
-        </li>
-        <li>
-          <button className="key">3</button>
-        </li>
-        <li>
-          <button className="key">4</button>
-        </li>
-        <li>
-          <button className="key">5</button>
-        </li>
-        <li>
-          <button className="key">6</button>
-        </li>
-        <li>
-          <button className="key">7</button>
-        </li>
-        <li>
-          <button className="key">8</button>
-        </li>
-        <li>
-          <button className="key">9</button>
-        </li>
-        <li>
-          <button className="key">0</button>
-        </li>
-        <li>
-          <button className="key big">delete</button>
+        {keyboardValues.map((value) => {
+          return (
+            <li key={value}>
+              <Key
+                className="key"
+                text={value}
+                action={() => addNumber(+value)}
+              />
+            </li>
+          );
+        })}
+        <li key={keyboardValues.length + 1}>
+          <Key className="key big" text="delete" action={() => {}} />
         </li>
       </ol>
     </div>
